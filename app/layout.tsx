b52a7dfdb7f34844130ccb8c60b596f2e4906484
Hedriz Admin <hedrizetalent2@gmail.com>
2025-12-05 21:08:12 +0700

    adding favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,19 +14,23 @@ export const metadata: Metadata = {
   generator: "v0.app",
   icons: {
     icon: [
+      // {
+      //   url: "/icon-light-32x32.png",
+      //   media: "(prefers-color-scheme: light)",
+      // },
+      // {
+      //   url: "/icon-dark-32x32.png",
+      //   media: "(prefers-color-scheme: dark)",
+      // },
+      // {
+      //   url: "/icon.svg",
+      //   type: "image/svg+xml",
+      // },
       {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
+        url: "/favicon.ico",
       },
     ],
+    shortcut: "/favicon.ico",
     apple: "/apple-icon.png",
   },
 }
